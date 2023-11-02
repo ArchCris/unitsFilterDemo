f57de2a13a821987db94203bf79551803a9e8139
arq.cristianmasci@gmail.com <arq.cristianmasci@gmail.com>
2023-11-02 22:30:27 +1100

    1

---
+++ app.js
@@ -0,0 +1,39 @@
+import {data} from './assets/data.js'
+
+const getInputs = () => {
+  let typeSelection = document.querySelector('input[name="type"]:checked').value
+  let widthSelection = Number(document.querySelector('input[name="width"]:checked').value)
+  let heightSelection = Number(document.querySelector('input[name="height"]:checked').value)
+  let depthSelection = Number(document.querySelector('input[name="depth"]:checked').value)
+  console.log(typeSelection,widthSelection)
+  handleChange([{type:typeSelection},{width:widthSelection},{height:heightSelection},{depth:depthSelection}])
+}
+
+document.getElementById("getUnits").addEventListener("click", getInputs);
+const outputTable = document.getElementById("outputTable")
+
+const handleChange = (array) => {
+  let result = null
+  for (let i = 0; i < array.length; i++) {
+    let property = Object.getOwnPropertyNames(array[i])[0]
+    console.log(property,array[i][property])
+    if(result==null){
+      result = data.filter(unidad=>unidad[property]===array[i][property])
+    }else{
+      result = result.filter(unidad=>unidad[property]===array[i][property])
+    }
+  }
+  result.forEach(node=>{
+    outputTable.innerHTML+=
+    `<tr>
+      <td>${node.code}</td>
+      <td>${node.width}</td>
+      <td>${node.height}</td>
+      <td>${node.depth}</td>
+      <td>${node.page}</td>
+    </tr>`
+    console.log(node)
+  })
+  console.log(result)
+}
+
---
+++ assets/data.js
@@ -0,0 +1,398 @@
+export const data = [
+  {
+    "code": "0L 250",
+    "width": 30,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 300",
+    "width": 35,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 350",
+    "width": 40,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 400",
+    "width": 45,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 450",
+    "width": 50,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 600",
+    "width": 80,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 650",
+    "width": 90,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 700",
+    "width": 100,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 850",
+    "width": 120,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 3
+  },
+  {
+    "code": "0L 472",
+    "width": 60,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_drawers",
+    "page": 3
+  },
+  {
+    "code": "0L 672",
+    "width": 90,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_drawers",
+    "page": 3
+  },
+  {
+    "code": "0L 872",
+    "width": 120,
+    "height": 72,
+    "depth": 35,
+    "type": "base_unit",
+    "open_system": "2_drawers",
+    "page": 3
+  },
+  {
+    "code": "01 250",
+    "width": 30,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 300",
+    "width": 40,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 350",
+    "width": 45,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 400",
+    "width": 50,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 450",
+    "width": 60,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 600",
+    "width": 80,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 650",
+    "width": 90,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 700",
+    "width": 100,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 850",
+    "width": 120,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 45S",
+    "width": 60,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "3_internal_drawers",
+    "page": 4
+  },
+  {
+    "code": "01 45S7",
+    "width": 60,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "1_door",
+    "inner": "3_internal_drawers",
+    "page": 4
+  },
+  {
+    "code": "01 65S",
+    "width": 90,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "3_internal_drawers",
+    "page": 4
+  },
+  {
+    "code": "01 85S",
+    "width": 120,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "3_internal_drawers",
+    "page": 4
+  },
+  {
+    "code": "01 65S7",
+    "width": 90,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "3_internal_drawers",
+    "page": 4
+  },
+  {
+    "code": "01 85S7",
+    "width": 120,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "2_doors",
+    "inner": "3_internal_drawers",
+    "page": 4
+  },
+  {
+    "code": "02 354",
+    "width": 45,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_drawer_1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "01 85S",
+    "width": 60,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_drawer_1_door",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "02 654",
+    "width": 90,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_drawer_2_doors",
+    "inner": "1_shelf",
+    "page": 4
+  },
+  {
+    "code": "02 357",
+    "width": 45,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_drawer_1_pull-out",
+    "page": 5
+  },
+  {
+    "code": "02 457",
+    "width": 60,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_drawer_1_pull-out",
+    "page": 5
+  },
+  {
+    "code": "02 657",
+    "width": 90,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "1_drawer_1_pull-out",
+    "page": 5
+  },
+  {
+    "code": "01 277",
+    "width": 30,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": "01 377",
+    "width": 45,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": "01 427",
+    "width": 50,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": "01 477",
+    "width": 60,
+    "height": 72,
+    "depth": 60,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": 320320,
+    "width": 30,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": 685563,
+    "width": 45,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": 868184,
+    "width": 50,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  },
+  {
+    "code": 1050805,
+    "width": 60,
+    "height": 72,
+    "depth": 70,
+    "type": "base_unit",
+    "open_system": "4_drawers",
+    "page": 5
+  }
+]
